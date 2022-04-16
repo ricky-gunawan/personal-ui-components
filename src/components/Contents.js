0aa6c966c38124baebbaad0386d811c0b09acd64
Ricky Indra Gunawan <rickyindra006@gmail.com>
2022-04-16 19:11:45 +0700

    edit avatar and add badge components

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Alert from "./contents/Alert";
 import Avatar from "./contents/Avatar";
+import Badge from "./contents/Badge";
 import Button from "./contents/Button";
 import Dropdown from "./contents/Dropdown";
 import Modal from "./contents/Modal";
@@ -15,6 +16,7 @@ const Contents = () => {
       <Swap />
       <Alert />
       <Avatar />
+      <Badge />
       {/* <div>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi iste, praesentium illum ipsum nostrum minima nesciunt iure itaque ab debitis omnis veritatis officiis saepe maiores aliquam voluptates cum tempore quae quod. Nulla aut
         ex totam modi facere! Aspernatur exercitationem quaerat quidem veritatis perferendis expedita, iure ipsum quod, aperiam architecto a.
--- src/components/contents/Avatar.js
+++ src/components/contents/Avatar.js
@@ -21,16 +21,16 @@ const Avatar = () => {
             <img src="https://source.unsplash.com/100x100/?face" alt="face" />
           </div>
           <div className="absolute left-4 h-20 w-20 overflow-clip rounded-full border-2 border-slate-500 text-center">
-            <img src="https://source.unsplash.com/100x100/?face" alt="face" />
+            <img src="https://source.unsplash.com/100x100/?road" alt="face" />
           </div>
           <div className="absolute left-8 h-20 w-20 overflow-clip rounded-full border-2 border-slate-500 text-center">
-            <img src="https://source.unsplash.com/100x100/?face" alt="face" />
+            <img src="https://source.unsplash.com/100x100/?foot" alt="face" />
           </div>
           <div className="absolute left-12 h-20 w-20 overflow-clip rounded-full border-2 border-slate-500 text-center">
-            <img src="https://source.unsplash.com/100x100/?face" alt="face" />
+            <img src="https://source.unsplash.com/100x100/?hand" alt="face" />
           </div>
           <div className="absolute left-16 h-20 w-20 overflow-clip rounded-full border-2 border-slate-500 text-center">
-            <img src="https://source.unsplash.com/100x100/?face" alt="face" />
+            <img src="https://source.unsplash.com/100x100/?head" alt="face" />
           </div>
         </div>
         <div className="relative flex h-20 flex-initial bg-red-500">
@@ -38,13 +38,13 @@ const Avatar = () => {
             <img src="https://source.unsplash.com/100x100/?face" alt="face" />
           </div>
           <div className="absolute left-4 h-20 w-20 overflow-clip rounded-full border-2 border-slate-500 text-center">
-            <img src="https://source.unsplash.com/100x100/?face" alt="face" />
+            <img src="https://source.unsplash.com/100x100/?road" alt="face" />
           </div>
           <div className="absolute left-8 h-20 w-20 overflow-clip rounded-full border-2 border-slate-500 text-center">
-            <img src="https://source.unsplash.com/100x100/?face" alt="face" />
+            <img src="https://source.unsplash.com/100x100/?foot" alt="face" />
           </div>
           <div className="absolute left-12 h-20 w-20 overflow-clip rounded-full border-2 border-slate-500 text-center">
-            <img src="https://source.unsplash.com/100x100/?face" alt="face" />
+            <img src="https://source.unsplash.com/100x100/?hair" alt="face" />
           </div>
           <div className="absolute left-16 flex h-20 w-20 justify-center overflow-clip rounded-full border-2 border-slate-500 bg-slate-700">
             <div className="self-center text-white">99+</div>

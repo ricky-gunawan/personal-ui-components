0113eb96203194af223ce4d8d6f87868d63f2185
Ricky Indra Gunawan <rickyindra006@gmail.com>
2022-07-30 13:59:49 +0700

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Web UI Components
-
-In this project, I built UI components using Tailwind CSS and ReactJS
-
-https://ricky-gunawan.github.io/personal-ui-components/
--- src/contents/syntaxHighlighter.js
+++
@@ -1,7 +0,0 @@
-import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import jsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
-import prism from "react-syntax-highlighter/dist/esm/styles/prism/prism";
-
-SyntaxHighlighter.registerLanguage("jsx", jsx);
-
-export { SyntaxHighlighter, prism };
